8297d5ce09ef0bcfcf48f5b22aa5f1c77052404a
Dana Rozanova <rozliz@outlook.com>
2026-05-06 13:50:31 +0100

    JS fix

--- src/scripts/hoverBorder.ts
+++ src/scripts/hoverBorder.ts
@@ -1,10 +1,10 @@
 document.querySelectorAll<HTMLElement>(".hover-border").forEach((el) => {
-  el.addEventListener("mousemove", (e) => {
-    const rect = el.getBoundingClientRect()
-    const x = e.clientX - rect.left
-    const y = e.clientY - rect.top
+  el.addEventListener("mousemove", (e: MouseEvent) => {
+    const rect = el.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
 
-    el.style.setProperty("--x", `${x}px`)
-    el.style.setProperty("--y", `${y}px`)
-  })
-})
+    el.style.setProperty("--x", `${x}px`);
+    el.style.setProperty("--y", `${y}px`);
+  });
+});
